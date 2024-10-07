140f9499daeba47998f85eab42fbd4aea325f900
Marchitos17 <marcluc1998@gmail.com>
2024-10-07 23:25:54 +0200

    Sistemare carrello

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -3,6 +3,85 @@
   $data = Carrello::all();
 ?>
 <style>
+body{
+    background-color: #eeeeee;
+}
+
+.footer-background{
+    background-color: rgb(204, 199, 199);
+}
+
+@media(max-width:991px){
+    #heading{
+        padding-left: 50px;
+    }
+    #prc{
+        margin-left: 70px;
+        padding-left: 110px;
+    }
+    #quantity{
+        padding-left: 48px;
+    }
+    #produc{
+        padding-left: 40px;
+    }
+    #total{
+        padding-left: 54px;
+    }
+}
+@media(max-width:767px){
+    .mobile{
+        font-size: 10px;
+    }
+    h5{
+        font-size: 14px;
+    }
+    h6{
+        font-size: 9px;
+    }
+    #mobile-font{
+        font-size: 11px;
+    }
+    #prc{
+        font-weight: 700;
+        margin-left: -45px;
+        padding-left: 105px;
+    }
+    #quantity{
+        font-weight: 700;
+        padding-left: 6px;
+    }
+    #produc{
+        font-weight: 700;
+        padding-left: 0px;
+    }
+    #total{
+        font-weight: 700;
+        padding-left: 9px;
+    }
+    #image{
+        width: 60px;
+        height: 60px;
+    }
+    .col{
+        width: 100%;
+    }
+    #zero-pad{
+        padding: 2%;
+        margin-left: 10px;
+    }
+    #footer-font{
+        font-size: 12px;
+    }
+    #heading{
+        padding-top: 15px;
+    }
+}
+
+
+
+
+
 a,
 a:active,
 a:focus,
@@ -169,29 +248,63 @@ a:hover {
                                         $totale = 0; 
                                       ?> 
                                       <div class="container">             
-                                              <div class="row">
-                                                    <div class="col-md-6 text-center">
-                                                      <h4 class="text-center mt-4">Carrello</h4>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <div class="flex center sec-center" style="height:80px;color:#fff;">
-                                                            <h6 id="txt" class="margin-5"></h6>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                @foreach ($data as $dataa)
-                                                  <div class="dropdown-cart-top-body p-1">
-                                                    <img class="img-fluid mr-3" alt="osahan" src="room/{{$dataa->immagine}}" style="width: 50px;">
-                                                    <p class="float-end text-body-secondary"> {{$dataa->room_nome}}</p>
-                                                    <p class="float-end text-body-secondary"> {{$dataa->room_prezzo}} </p>
+                                        <div class="bg-white rounded-top" id="zero-pad">
+                                          <div class="row d-flex justify-content-center">
+                                              <div class="col-lg-10 col-12 pt-3">
+                                                  <div class="d-flex">
+                                                      <div class="pt-1"><h4>Carrello</h4></div>
                                                   </div>
-                                                @endforeach
-                                                <div class="align-center">
-                                                  <a class="sidenav__menu-item text-black text-center bg-body-secondary" href="#">Paga</a>
-                                                  <a class="sidenav__menu-item text-black text-center" href="#">Vedi il carrello</a>
-                                                </div>
-                                            </div>
+                                                  <div class="d-flex flex-column pt-4">
+                                                      <div class="font-weight-normal">2 items</div>
+                                                  </div>
+                                                  <div class="d-flex flex-row px-lg-5 mx-lg-5 mobile" id="heading">
+                                                      <div class="px-lg-5 mr-lg-5" id="produc">PRODUCTS</div>
+                                                      <div class="px-lg-5 ml-lg-5" id="prc">PRICE</div>
+                                                      <div class="px-lg-5 ml-lg-1" id="quantity">QUANTITY</div>
+                                                      <div class="px-lg-5 ml-lg-3" id="total">TOTAL</div>
+                                                  </div>
+                                                  @foreach ($data as $dataa)
+                                                  <?php $totale= $totale + $dataa->room_prezzo; ?>
+                                                  <div class="d-flex flex-row justify-content-between align-items-center pt-lg-4 pt-2 pb-3 border-bottom mobile">
+                                                      <div class="d-flex flex-row align-items-center">
+                                                          <div><img src="room/{{$dataa->immagine}}" width="150" height="150" alt="" id="image"></div>
+                                                          <div class="d-flex flex-column pl-md-3 pl-1">
+                                                              <div><h6>{{$dataa->room_nome}}</h6></div>
+                                                              <div >Art.No:<span class="pl-2">091091001</span></div>
+                                                              <div>Color:<span class="pl-3">White</span></div>
+                                                              <div>Size:<span class="pl-4"> M</span></div>
+                                                          </div>                    
+                                                      </div>
+                                                      <div class="pl-md-0 pl-1"><b>€{{$dataa->prezzo}}</b></div>
+                                                      <div class="pl-md-0 pl-2">
+                                                          <span class="fa fa-minus-square text-secondary"></span><span class="px-md-3 px-1">2</span><span class="fa fa-plus-square text-secondary"></span>
+                                                      </div>
+                                                      <div class="pl-md-0 pl-1"><b>$19.98</b></div>
+                                                      <div class="close">&times;</div>
+                                                  </div>
+                                                  @endforeach
+
+                                      
+                                              </div>
                                           </div>
+                                      </div>
+                                      <div class="container bg-light rounded-bottom py-4" id="zero-pad">
+                                          <div class="row d-flex justify-content-center">
+                                              <div class="col-lg-10 col-12">
+                                                  <div class="d-flex justify-content-between align-items-center">
+                                                      <div>
+                                                          <button class="btn btn-sm bg-light border border-dark">GO BACK</button>
+                                                      </div>
+                                                      <div class="px-md-0 px-1" id="footer-font">
+                                                          <b class="pl-md-4">SUBTOTAL<span class="pl-md-4">$61.78</span></b>
+                                                      </div>
+                                                      <div>
+                                                          <button class="btn btn-sm bg-dark text-white px-lg-5 px-3">CONTINUE</button>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                      </div>
                                       <!--<div class="dropdown-menu dropdown-cart-top p-0 dropdown-menu-left shadow-sm border-0">
                                         <div class="dropdown-cart-top-footer p-1 text-center">
                                           <p class="mb-0 font-weight-bold text-secondary"><h4>Carrello</h4></p>
@@ -267,7 +380,7 @@ a:hover {
         if ($(window).width() < 601) {
             document.getElementById("mySidenav").style.width = "40%";
         } else {
-            document.getElementById("mySidenav").style.width = "30%";
+            document.getElementById("mySidenav").style.width = "60%";
             document.getElementById("open-menu").style.display = "none";
         }
     }
